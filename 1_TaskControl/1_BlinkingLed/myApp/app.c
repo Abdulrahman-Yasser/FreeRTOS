31b83335f20e2e4b671d1af82112bcae9fdafa70
Abdulrahman Yasser <abdu.ya74@gmail.com>
2023-09-29 23:18:56 +0300

    the blinking led works fine FINALLY

--- 1_TaskControl/1_BlinkingLed/myApp/app.c
+++ 1_TaskControl/1_BlinkingLed/myApp/app.c
@@ -8,12 +8,12 @@ static char* task_1_name = "First";
 
 void Task1_blinking(void* pvParameter){
     TickType_t xLastWakeTime;
-    xLastWakeTime = xTaskGetTickCount();
     DigitalInterface_Type* myLed_1 = Digital_Interface_Create(DIO_Channel2_F);
+    xLastWakeTime = xTaskGetTickCount();
     while(1){
         myLed_1->Write_Toggle(myLed_1);
-        vTaskDelay(1000);
-//        vTaskDelayUntil(xLastWakeTime, 1000);
+//        vTaskDelay(1000);
+        vTaskDelayUntil(&xLastWakeTime, 1000);
     }
 }
 
